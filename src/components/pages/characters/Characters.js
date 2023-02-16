83335842b43cfba83a7b3ce112701af8809c7635
Andrew Cook <andrewjcook93@gmail.com>
2023-02-15 20:16:49 -0600

    finish player character cards

--- src/components/pages/characters/Characters.js
+++ src/components/pages/characters/Characters.js
@@ -17,20 +17,19 @@ function Characters() {
             <div className="card__side card__side--front">
               <div className="card__picture card__picture--1">&nbsp;</div>
               <div className="card__heading">Safzira &nbsp;</div>
-            </div> {/* card__side End */}
+            </div> {/* card__side--front End */}
             <div className="card__side card__side--back">
               <div className="card__description">
                 <div className="card__description--top">
                   Rogue Lvl 4
                   <span>Arcane Trickster</span>
-                </div>
+                </div> {/* card__description--top End */}
                 <div className="card__description--bottom">
                 Safzira might have a greater body count than the Barbarian, but in a totally different way. She is well known for her unpredictable fighting style and close relationships.
-                </div>
-              </div>
-
+                </div> {/* card__description--bottom End */}
+              </div> {/* card__description End */}
               <div className="card__heading">Safzira &nbsp;</div>
-            </div> {/* card__side End */}
+            </div> {/* card__side--back End */}
           </div> {/* card End */}
         </div> {/* Safzira End */}
 
@@ -39,7 +38,19 @@ function Characters() {
             <div className="card__side card__side--front">
               <div className="card__picture card__picture--2">&nbsp;</div>
               <div className="card__heading">Elizana &nbsp;</div>
-            </div> {/* card__side End */}
+            </div> {/* card__side--front End */}
+            <div className="card__side card__side--back">
+              <div className="card__description">
+                <div className="card__description--top">
+                  Rogue Lvl 5
+                  <span>Thief</span>
+                </div> {/* card__description--top End */}
+                <div className="card__description--bottom">
+                Elizana is known for her kindness and curiousity. Her spirits have been tested greatly since arriving in Barovia.
+                </div> {/* card__description--bottom End */}
+              </div> {/* card__description End */}
+              <div className="card__heading">Elizana &nbsp;</div>
+            </div> {/* card__side--back End */}
           </div> {/* card End */}
         </div> {/* Elizana End */}
 
@@ -49,8 +60,62 @@ function Characters() {
               <div className="card__picture card__picture--3">&nbsp;</div>
               <div className="card__heading">German &nbsp;</div>
             </div> {/* card__side End */}
+            <div className="card__side card__side--back">
+              <div className="card__description">
+                <div className="card__description--top">
+                  Barbarian Lvl 4
+                  <span>Zealot</span>
+                </div> {/* card__description--top End */}
+                <div className="card__description--bottom">
+                German the Barbarian, a man of few words, lives for battle. He will protect his fellow Sunseekers to the death just for the thrill of a fight.
+                </div> {/* card__description--bottom End */}
+              </div> {/* card__description End */}
+              <div className="card__heading">German &nbsp;</div>
+            </div> {/* card__side--back End */}
           </div> {/* card End */}
         </div> {/* German End */}
+
+        <div className="flexbox__item"> {/* Damani Start */}
+          <div className="card">
+            <div className="card__side card__side--front">
+              <div className="card__picture card__picture--4">&nbsp;</div>
+              <div className="card__heading">Umnos &nbsp;</div>
+            </div> {/* card__side End */}
+            <div className="card__side card__side--back">
+              <div className="card__description">
+                <div className="card__description--top">
+                  Cleric Lvl 5
+                  <span>Forge Domain</span>
+                </div> {/* card__description--top End */}
+                <div className="card__description--bottom">
+                Discovered in a Vallaki prison healing Lars Kjurls, Umnos soon joined the Sunseekers after Ireena was saved from the wolves.
+                </div> {/* card__description--bottom End */}
+              </div> {/* card__description End */}
+              <div className="card__heading">Umnos &nbsp;</div>
+            </div> {/* card__side--back End */}
+          </div> {/* card End */}
+        </div> {/* Damani End */}
+
+        <div className="flexbox__item"> {/* Margarito Start */}
+          <div className="card">
+            <div className="card__side card__side--front">
+              <div className="card__picture card__picture--5">&nbsp;</div>
+              <div className="card__heading">Zoro &nbsp;</div>
+            </div> {/* card__side End */}
+            <div className="card__side card__side--back">
+              <div className="card__description">
+                <div className="card__description--top">
+                  Fighter Lvl 4
+                  <span>subclass</span>
+                </div> {/* card__description--top End */}
+                <div className="card__description--bottom">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam imperdiet feugiat nulla, bibendum tincidunt lectus venenatis pellentesque.
+                </div> {/* card__description--bottom End */}
+              </div> {/* card__description End */}
+              <div className="card__heading">Zoro &nbsp;</div>
+            </div> {/* card__side--back End */}
+          </div> {/* card End */}
+        </div> {/* Margarito End */}
 
         
       </div> {/* row End */}
