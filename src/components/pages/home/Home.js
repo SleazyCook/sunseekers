a000a75fe2c0c806992a8b74ea6394d19dca2396
Andrew Cook <andrewjcook93@gmail.com>
2023-02-18 16:23:08 -0600

    add home animations

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -5,14 +5,14 @@ function Home() {
   return (
     <div className="home">
 
-      <div className="header__textbox">
+      <div className="home__textbox">
         <h1 className="heading-primary">
           <span className="heading-primary--main">Curse of Strahd</span>
           <span className="heading-primary--sub">Plight of the Sunseekers</span>
         </h1>
         <br />
         <br />
-        <Link to="/characters" className="btn">
+        <Link to="/notes" className="btn">
           Guide</Link>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <a href="https://death-house.netlify.app" className="btn">
