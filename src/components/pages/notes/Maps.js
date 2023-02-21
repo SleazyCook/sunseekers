6ead00924e7cf8cd48c7a0aea101bc80c871ff48
Andrew Cook <andrewjcook93@gmail.com>
2023-02-21 10:44:34 -0600

    add flexbox__item center, ul float left to map keys

--- src/components/pages/notes/Maps.js
+++ src/components/pages/notes/Maps.js
@@ -22,7 +22,7 @@ function Maps() {
 
       <p className="strahd-descriptive">
         The following took maps have been secured by the Sunseekers during their travels across Barovia.
-      </p><br />
+      </p>
       
       <h3 className="heading-tertiary u-margin-bottom-small">Maps of Barovia</h3>
 
@@ -43,14 +43,17 @@ function Maps() {
         <img src="https://i.imgur.com/zxl3W65.png" className="maps-box__map" />
         <p className="typewriter-script u-center-text">
           A Map of the Village of Barovia</p><br />
-          <ul className="maps-box__list">
-            <li>E1. Bildrath's Mercantile</li>
-            <li>E2. Blood of the Vine Tavern</li>
-            <li>E3. Mad Mary's Townhouse</li>
-            <li>E4. Burgomaster's Mansion</li>
-            <li>E5. Church</li>
-            <li>E6. Cemetary</li>
-          </ul>
+          <div className="flexbox">
+              <ul className="maps-box__list flexbox__item">
+                <li>E1. Bildrath's Mercantile</li>
+                <li>E2. Blood of the Vine Tavern</li>
+                <li>E3. Mad Mary's Townhouse</li>
+                <li>E4. Burgomaster's Mansion</li>
+                <li>E5. Church</li>
+                <li>E6. Cemetary</li>
+              </ul>
+          </div> {/* Flexbox End */}
+
         <br />
 
         <img src="https://i.imgur.com/fxgQqfY.jpg" className="maps-box__map"/>
@@ -64,17 +67,19 @@ function Maps() {
         <img src="https://i.imgur.com/LOLy8XV.png" className="maps-box__map" />
         <p className="typewriter-script u-center-text">
           A Map of the Vallaki</p><br />
-          <ul className="maps-box__list">
-            <li>N1. St. Andral's Church</li>
-            <li>N2. Blue Water Inn</li>
-            <li>N3. Burgomaster's Mansion</li>
-            <li>N4. Wachterhaus</li>
-            <li>N5. Arasek Stockyard</li>
-            <li>N6. Coffin Maker's Shop</li>
-            <li>N7. Blinsky's Toys</li>
-            <li>N8. Town Square</li>
-            <li>N9. Vistani Camp</li>
-          </ul>
+          <div className="flexbox">
+            <ul className="maps-box__list flexbox__item">
+              <li>N1. St. Andral's Church</li>
+              <li>N2. Blue Water Inn</li>
+              <li>N3. Burgomaster's Mansion</li>
+              <li>N4. Wachterhaus</li>
+              <li>N5. Arasek Stockyard</li>
+              <li>N6. Coffin Maker's Shop</li>
+              <li>N7. Blinsky's Toys</li>
+              <li>N8. Town Square</li>
+              <li>N9. Vistani Camp</li>
+            </ul>
+          </div> {/* Flexbox End */}
         <br />
 
         <img src="https://i.imgur.com/Oia2nrv.jpg" className="maps-box__map"/>
