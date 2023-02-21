873917d69d1a5087cc02331c87384f122692df7d
Andrew Cook <andrewjcook93@gmail.com>
2023-02-21 02:56:21 -0600

    add all tarroka cards and scripts

--- src/components/pages/notes/TarrokaReading.js
+++ src/components/pages/notes/TarrokaReading.js
@@ -21,6 +21,10 @@ function TarrokaReading() {
       </div>
 
       <p className="strahd-descriptive">
+        [The following took place inside the fortune teller Madame Eva's tent at the Vistani Tser Pool Encampment on the 14th Evening of Hammer]<br /><br />
+
+        <div className="u-thin-border-top">&nbsp;</div>
+
         Her old hands working deftly, the ancient seer removes fourteen cards from the top of the deck, setting them aside. The remaining cards, she shuffles nimbly twice, three times, four.<br /><br />
 
         Madame Eva sets both decks upon the surface of the velvet table. Closing her eyes, she places her right hand over the surface of the larger deck. The crimson flames dim and swirl in eldritch patterns as her lips move silently, a distant tension spreading through the air. The sounds of the rustling trees and rippling pool beyond the tent's walls begin to dim, the external world growing mute and insubstantial as the space within grows more solid - more real.<br /><br />
@@ -31,7 +35,11 @@ function TarrokaReading() {
 
         The crone then moves her wrinkled hand to the left-most card - the first. She closes her eyes and tilts her head, as if listening to an unspoken word. The arcane lights swirl and then shift, their colors changing to a deep, piercing blue. ""This card tells of history. Knowledge of the ancient will help you better understand your enemy.<br /><br />
 
-        She flips the card.
+        She flips the card.<br /><br />
+
+        The cerulean light dances across its surface, revealing an illustration of a powerful sorceress extends her four arms. One hand clasps a torch, one a glowing orb, and the final two contain a single coin each.<br /><br />
+
+        "The Eight of glyphs - the Transmuter" Her dark pupils shift from side to side, as though reading from an unseen text. "Go to a place of dizzying heights, where the stone itself is alive!"
 
       </p>
 
@@ -54,7 +62,7 @@ function TarrokaReading() {
             The Transmuter</h3>
             <p className="tarroka-script u-center-text">
             "This card tells of history. Knowledge of the ancient will help you better understand your enemy." <br />
-            Go to a place of dizzying heights, where the stone itself is alive. 
+            Go to a place of dizzying heights, where the stone itself is alive!
             </p>
             <br /><br />
             <p className="typewriter-script u-center-text">
@@ -65,13 +73,25 @@ function TarrokaReading() {
 
       </div> {/* transmuter end */}
 
+      <p className="strahd-descriptive">
+        She moves her hand to the second card, this one at the top of the cross. As she closes her eyes and listens once more, the candlelight flares, its color bursting into a fierce, cheery yellow.<br /><br />
 
-      <div className="flexbox flexbox--tarroka"> {/* transmuter start */}
+        "This card tells of a powerful force for good and protection, a holy symbol of great hope."<br /><br />
+
+        She flips the card. <br /><br />
+
+        This time, the light reveals a new illustration: a portly man about to pour himself another drink from the cask under his arm. <br /><br />
+
+        "The Three of coins - the Monk". Her eyes stare deep into the shadows that lurk in the corners of the tent. "The treasure you seek is hidden behind the sun, in the house of a saint."
+      </p>
+
+
+      <div className="flexbox flexbox--tarroka"> {/* monk start */}
 
         <div className="flexbox__item"> 
           <div className="tarroka-card">
             <div className="tarroka-card__side tarroka-card__side--front">
-              <img src="https://i.imgur.com/i29osPB.png" />
+              <img src="https://i.imgur.com/jO6ak2e.png" />
             </div> {/* side--front end */}
             <div className="tarroka-card__side tarroka-card__side--back">
               <img src="https://i.imgur.com/i5jBvTr.png" />
@@ -81,21 +101,151 @@ function TarrokaReading() {
 
         <div className="flexbox__item flexbox__item--tarroka">
           <h3 className="heading-tertiary">
-            <u>The Eight of Glyphs</u><br />
-            The Transmuter</h3>
+            <u>The Three of Coins</u><br />
+            The Monk</h3>
             <p className="tarroka-script u-center-text">
-            "This card tells of history. Knowledge of the ancient will help you better understand your enemy." <br />
-            Go to a place of dizzying heights, where the stone itself is alive. 
+            "This card tells of a powerful force for good and protection, a holy symbol of great hope." <br />
+            The treasure you seek is hidden behind the sun, in the house of a saint.
+            </p>
+            <br /><br />
+            <p className="typewriter-script u-center-text">
+              {/* UPDATE:<br/>
+              The Tome of Strahd was found atop a tall tower within Castle Ravenloft. */}
+            </p>
+        </div> 
+
+      </div> {/* monk end */}
+
+      <p className="strahd-descriptive">
+        She moves her hand to the third card, at the right arm of the cross, her eyelids closing like a trance, her lips pursed in quiet contemplation. The candlelight vanishes, for a heartbeat - and then returns in a nova of fierce, burning white, so pure and strong and clean that it hurts to look at, burns to see—<br /><br />
+
+        Madam Eva's eyes snap open, burning with a fierce determination. ""This is a card of power and strength. It tells of a weapon of vengeance: a sword of sunlight.""<br /><br />
+
+        She flips the card.<br /><br />
+
+        The light reveals a third illustration: a mysterious scowling figure grips the pommel of a sword pointed at the ground.  "The Seven of stars - the Hooded One". The crone's voice is strong with purpose. "I see a faceless god. He awaits you at the end of a long and winding road, deep in the mountains."
+      </p>
+
+      <div className="flexbox flexbox--tarroka"> {/* hooded one start */}
+
+      <div className="flexbox__item"> 
+        <div className="tarroka-card">
+          <div className="tarroka-card__side tarroka-card__side--front">
+            <img src="https://i.imgur.com/KuAj40b.png" />
+          </div> {/* side--front end */}
+          <div className="tarroka-card__side tarroka-card__side--back">
+            <img src="https://i.imgur.com/i5jBvTr.png" />
+          </div> {/* side--back end */}
+        </div> {/* card end */}
+      </div> 
+
+      <div className="flexbox__item flexbox__item--tarroka">
+        <h3 className="heading-tertiary">
+          <u>The Seven of Stars</u><br />
+          The Hooded One</h3>
+          <p className="tarroka-script u-center-text">
+          "This is a card of power and strength. It tells of a weapon of vengeance; a sword of sunlight." <br />
+          I see a faceless god. He awaits you at the end of a long and winding road, deep in the mountains. 
+          </p>
+          <br /><br />
+          <p className="typewriter-script u-center-text">
+            {/* UPDATE:<br/>
+            The Tome of Strahd was found atop a tall tower within Castle Ravenloft. */}
+          </p>
+      </div> 
+
+      </div> {/* hooded one end */}
+
+      <p className="strahd-descriptive">
+        She moves to the fourth card, at the bottom of the cross, and listens once more, tracing small circles across its back as she hums a contemplative note. The magic flames leap and dance upon their wicks, now casting swirling violet embers into the air as the walls of the tent gleam with the shimmer of twilight.<br /><br />
+
+        "This card sheds light on one who will help you greatly in the battle against darkness."<br /><br />
+
+        She flips the card.<br /><br />
+
+        This time, the illustration revealed is a strange decorated jar contains a human skull with a dark, hollow gaze. Its scalp is carved with symbols of profane arcanum.<br /><br />
+
+        "The Joker - the Artifact". Madam Eva leans forward with a hushed intensity to her voice "Look for an entertaining man with a monkey. This man is more than he seems."
+      </p>
+
+      <div className="flexbox flexbox--tarroka"> {/* artifact start */}
+
+        <div className="flexbox__item"> 
+          <div className="tarroka-card">
+            <div className="tarroka-card__side tarroka-card__side--front">
+              <img src="https://i.imgur.com/LN8PmRe.png" />
+            </div> {/* side--front end */}
+            <div className="tarroka-card__side tarroka-card__side--back">
+              <img src="https://i.imgur.com/i5jBvTr.png" />
+            </div> {/* side--back end */}
+          </div> {/* card end */}
+        </div> 
+
+        <div className="flexbox__item flexbox__item--tarroka">
+          <h3 className="heading-tertiary">
+            <u>The Joker</u><br />
+            The Artifact</h3>
+            <p className="tarroka-script u-center-text">
+            "This card sheds light on one who will help you greatly in the battle against darkness" <br />
+            Look for an entertaining man with a monkey. This man is more than he seems.  
             </p>
             <br /><br />
             <p className="typewriter-script u-center-text">
               UPDATE:<br/>
-              The Tome of Strahd was found atop a tall tower within Castle Ravenloft.
+              Famed monster hunter Rudolph van Richten was in Vallaki posing as a bard with a pet monkey. 
             </p>
         </div> 
 
-      </div> {/* transmuter end */}
+      </div> {/* artifact end */}
 
+      <p className="strahd-descriptive">
+        Finally, she moves her hand to the fifth card - and nearly recoils, her brow furrowing until the wrinkles split her forehead like a trench. Behind her, shadows encircle the candlelight until the light is very nearly swallowed by the creeping dark.<br /><br />
+
+        When next she speaks, Madam Eva's rasping voice is scarce above a whisper. "Your enemy is a creature of darkness, whose powers are beyond mortality. This card will lead you to him!"<br /><br />
+
+        Her hand trembles above the card for a silent moment - and then deft, ancient fingers reveal its opposite side.<br /><br />
+
+        "In the darkness, the fifth and final illustration is only barely visible through the smoke and unnatural murk:<br />
+        a grotesque beast with menacing fangs and long claws rips apart an iron fence, a village sleeps peacefully behind it Madam Eva slowly exhales."The Jester - the Beast. The beast sits on his dark throne.
+      </p>
+
+      <div className="flexbox flexbox--tarroka"> {/* beast start */}
+
+        <div className="flexbox__item"> 
+          <div className="tarroka-card">
+            <div className="tarroka-card__side tarroka-card__side--front">
+              <img src="https://i.imgur.com/QabQGME.png" />
+            </div> {/* side--front end */}
+            <div className="tarroka-card__side tarroka-card__side--back">
+              <img src="https://i.imgur.com/i5jBvTr.png" />
+            </div> {/* side--back end */}
+          </div> {/* card end */}
+        </div> 
+
+        <div className="flexbox__item flexbox__item--tarroka">
+          <h3 className="heading-tertiary">
+            <u>The Jester</u><br />
+            The Beast</h3>
+            <p className="tarroka-script u-center-text">
+            "Your enemy is a creature of darkness, whose powers are beyond mortality. This card will lead you to him!" <br />
+            The beast sits on his dark throne. 
+            </p>
+            <br /><br />
+            <p className="typewriter-script u-center-text">
+              {/* UPDATE:<br/>
+              Famed monster hunter Rudolph van Richten was in Vallaki posing as a bard with a pet monkey.  */}
+            </p>
+        </div> 
+
+      </div> {/* beast end */}
+
+      <p className="strahd-descriptive">
+        As the last syllable passes her lips, the old woman freezes - and then rocks back in her chair, her eyes rolling until their whites gleam like pearls in the darkness - and then she snaps back, the candlelight burning down to its customary crimson glow.<br /><br />
+
+        The sound of the outside world returns - the voices of the Vistani, the crackling of the bonfire, the whisper of the wind, and the lapping of the waves against the shore of the pool. Light, grey and insubstantial, filters in once more through the canvas walls of the tent, and you feel yourselves breathe for the first time since the reading began.<br /><br />
+
+        Madam Eva says nothing. She only regards you silently with dark, heavy eyes.
+      </p>
 
       <Footer />
 
