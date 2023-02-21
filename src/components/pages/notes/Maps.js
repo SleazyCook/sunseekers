633df27dcbe4efb67d9e442391eceadae30f3bad
Andrew Cook <andrewjcook93@gmail.com>
2023-02-21 10:31:20 -0600

    add all maps for barovia, village of barovia, vallaki

--- src/components/pages/notes/Maps.js
+++ src/components/pages/notes/Maps.js
@@ -22,19 +22,64 @@ function Maps() {
 
       <p className="strahd-descriptive">
         The following took maps have been secured by the Sunseekers during their travels across Barovia.
-      </p>
+      </p><br />
       
-      <h3 className="heading-tertiary">Maps of Barovia</h3>
+      <h3 className="heading-tertiary u-margin-bottom-small">Maps of Barovia</h3>
 
       <div className="maps-box u-padding-medium">
         <img src="https://i.imgur.com/Lz5FS1r.jpg" className="maps-box__map" />
         <p className="typewriter-script u-center-text">
           Map of Barovia found in Castle Ravenloft</p>
         <br />
-        
+
         <img src="https://i.imgur.com/28ypbbJ.jpg" className="maps-box__map"/>
         <p className="typewriter-script u-center-text">
           Map of Barovia found in Vallaki</p>
+      </div>
+
+      <h3 className="heading-tertiary u-margin-bottom-small">Maps of the Village of Barovia</h3>
+
+      <div className="maps-box u-padding-medium">
+        <img src="https://i.imgur.com/zxl3W65.png" className="maps-box__map" />
+        <p className="typewriter-script u-center-text">
+          A Map of the Village of Barovia</p><br />
+          <ul className="maps-box__list">
+            <li>E1. Bildrath's Mercantile</li>
+            <li>E2. Blood of the Vine Tavern</li>
+            <li>E3. Mad Mary's Townhouse</li>
+            <li>E4. Burgomaster's Mansion</li>
+            <li>E5. Church</li>
+            <li>E6. Cemetary</li>
+          </ul>
+        <br />
+
+        <img src="https://i.imgur.com/fxgQqfY.jpg" className="maps-box__map"/>
+        <p className="typewriter-script u-center-text">
+          Map of Village Church</p>
+      </div>
+
+      <h3 className="heading-tertiary u-margin-bottom-small">Maps of Vallaki</h3>
+
+      <div className="maps-box u-padding-medium">
+        <img src="https://i.imgur.com/LOLy8XV.png" className="maps-box__map" />
+        <p className="typewriter-script u-center-text">
+          A Map of the Vallaki</p><br />
+          <ul className="maps-box__list">
+            <li>N1. St. Andral's Church</li>
+            <li>N2. Blue Water Inn</li>
+            <li>N3. Burgomaster's Mansion</li>
+            <li>N4. Wachterhaus</li>
+            <li>N5. Arasek Stockyard</li>
+            <li>N6. Coffin Maker's Shop</li>
+            <li>N7. Blinsky's Toys</li>
+            <li>N8. Town Square</li>
+            <li>N9. Vistani Camp</li>
+          </ul>
+        <br />
+
+        <img src="https://i.imgur.com/Oia2nrv.jpg" className="maps-box__map"/>
+        <p className="typewriter-script u-center-text">
+          Map of St. Andral's Church</p>
       </div>
 
 
