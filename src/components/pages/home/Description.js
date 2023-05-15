3bdafc13d46237be7537f478484a72a091adfd13
Andrew Cook <andrewjcook93@gmail.com>
2023-05-15 17:17:55 -0500

    rearrange: description title

--- src/components/pages/home/Description.js
+++ src/components/pages/home/Description.js
@@ -4,12 +4,14 @@ function Description() {
   return (
     <div className="description">
 
-      <div className="heading-tertiary">Welcome to Curse of Strahd: Plight of the Sunseekers</div>
+      
 
       <div className="description__flex-container">
 
         <div className="description__text-box">
+      
           <p className="description__text-actual">
+          <div className="heading-tertiary">The Plight for the Sunlight</div>
             Curse of Strahd: Plight of the Sunseekers is a virtual tabletop based on a popular 5E Dungeons and Dragons campagin. Many adventurers have stumbled into the dread domain of Barovia only to meet their fate at the hands of the vampie lord Count Strahd Von Zarovich or some other twisted horror. Those who remain continue to seek out the sun and make it back home alive. 
             <br /><br />
             The adventures of the Sunseekers so far are detailed in the chapters below.
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,14 +1,19 @@
 import AdventureLog from '../notes/AdventureLog';
 import Title from './Title';
 import Description from './Description';
+import Header from '../../Header'
 
 function Home() {
   return (
     <div>
 
-      <Title />
+      <Header />
+
+      <br />
 
       <Description />
+
+      <div className="heading-tertiary">The Adventure So Far...</div>
 
       <AdventureLog />
     </div>
