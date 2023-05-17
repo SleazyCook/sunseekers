292e854da972c0c17f374862aa3e8c56ffbc81c4
Andrew Cook <andrewjcook93@gmail.com>
2023-05-16 22:07:22 -0500

    add: maintenance message on homepage.

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -3,13 +3,28 @@ import Title from './Title';
 import Description from './Description';
 import Header from '../../Header'
 import Navigation from '../../Navigation'
+import { FaAlignCenter } from 'react-icons/fa';
 
 function Home() {
+
+  const myStle = {
+    color: "red",
+    textalign: "center"
+  }
   return (
     <div>
 
       <Header />
       <Navigation />
+
+      <div style={{color: "red", textalign: "center"}}>
+        <br />
+        <br />
+        Hello! This website is currently undergoing some work. I'm hoping to have it back up and running with more features and a new design by the end of May 2023. Feel free to poke around in the meantime.
+        <br />
+        <br />
+        -Drewford
+      </div>
 
       <br />
 
