0d95180506691cee0359ecd3bb84d7a440525f5a
Andrew Cook <andrewjcook93@gmail.com>
2023-05-18 14:46:56 -0500

    add: description background image

--- src/components/pages/home/DeathHouse.js
+++ src/components/pages/home/DeathHouse.js
@@ -4,6 +4,12 @@ function DeathHouse() {
   return (
     <div className="death-house">
       Death House Placeholder
+
+      <div className="description__game">
+        <a href="https://death-house.netlify.app" target="_blank" className="description__game-link">
+          <img className="description__game-img" src="https://i.imgur.com/j8zRB5s.png" />
+        </a>
+      </div>
     </div>
   )
 }
--- src/components/pages/home/Description.js
+++ src/components/pages/home/Description.js
@@ -6,14 +6,12 @@ function Description() {
 
       {/* New Take on This: Divide component into three columns. Text in the middle, an image on each side. Raven on one side, ravenloft crest on the other perhaps? */}
 
-      <div className="heading-tertiary">Tabletop Companion Guide & Interactive Game</div>
-
       <div className="description__flex-container">
 
         <div className="description__text-box">
       
           <p className="description__text-actual">
-            <div className="headline">The Plight for the Sunlight</div>
+            <div className="heading-tertiary description__headline ">The Plight for the Sunlight</div>
             <br />
 
             Curse of Strahd: Plight of the Sunseekers is a virtual tabletop based on a popular 5E Dungeons and Dragons campaign. Many adventurers have stumbled into the dread domain of Barovia only to meet their fate at the hands of the vampie lord Count Strahd Von Zarovich or some other twisted horror. Those who remain continue to seek out the sun and make it back home alive. 
@@ -22,13 +20,6 @@ function Description() {
           </p>
           <br />
         </div>
-
-        <div className="description__game">
-          <a href="https://death-house.netlify.app" target="_blank" className="description__game-link">
-            <img className="description__game-img" src="https://i.imgur.com/j8zRB5s.png" />
-          </a>
-        </div>
-
 
       </div> {/* End of Flex Container */}
 
