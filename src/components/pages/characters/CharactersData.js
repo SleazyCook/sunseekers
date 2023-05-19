c7ef17cd99fd3a30060c56047d8a40dcb3225d84
Andrew Cook <andrewjcook93@gmail.com>
2023-05-19 17:05:41 -0500

    fix: characterData link paths

--- src/components/pages/characters/CharactersData.js
+++ src/components/pages/characters/CharactersData.js
@@ -2,72 +2,72 @@ let charactersData = [
   {
     number: 1,
     name: "Sunseekers",
-    link: "/notes/characters/sunseekers"
+    link: "/characters/sunseekers"
   },
   {
     number: 2,
     name: "Castle Ravenloft",
-    link: "/notes/characters/castle-ravenloft"
+    link: "/characters/castle-ravenloft"
   },
   {
     number: 3,
     name: "Village of Barovia",
-    link: "/notes/characters/village-of-barovia"
+    link: "/characters/village-of-barovia"
   },
   {
     number: 4,
     name: "Vistani",
-    link: "/notes/characters/vistani"
+    link: "/characters/vistani"
   },
   {
     number: 5,
     name: "Keepers of the Feather",
-    link: "/notes/characters/keepers-of-the-feather"
+    link: "/characters/keepers-of-the-feather"
   },
   {
     number: 6,
     name: "Order of the Silver Dragon",
-    link: "/notes/characters/order-of-the-silver-dragon"
+    link: "/characters/order-of-the-silver-dragon"
   },
   {
     number: 7,
     name: "Church of the Morning Lord",
-    link: "/notes/characters/church-of-the-morninglord"
+    link: "/characters/church-of-the-morninglord"
   },
   {
     number: 8,
     name: "The Baron",
-    link: "/notes/characters/the-baron"
+    link: "/characters/the-baron"
   },
   {
     number: 9,
     name: "Wachterhaus",
-    link:"/notes/characters/wachterhaus"
+    link:"/characters/wachterhaus"
   },
   {
     number: 10,
     name: "Vallaki Merchants",
-    link: "/notes/characters/vallaki-merchants"
+    link: "/characters/vallaki-merchants"
   },
   {
     number: 11,
     name: "Vallaki Citizens",
-    link: "/notes/characters/vallaki-citizens"
+    link: "/characters/vallaki-citizens"
   },
   {
     number: 12,
     name: "Witches & Cults",
-    link: "/notes/characters/witches-and-cults"
+    link: "/characters/witches-and-cults"
   },
   {
     number: 13,
     name: "Unknown Alliance",
-    link: "/notes/characters/unknown-alliance"
+    link: "/characters/unknown-alliance"
   },
   {
     number: 14,
     name: "Creatures",
-    link: "/notes/characters/creatures"
+    link: "/characters/creatures"
   }
 ]
 
