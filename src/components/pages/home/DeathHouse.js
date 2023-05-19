ef143bb74ab9a90517ac7083beaf9b138fbbe55a
Andrew Cook <andrewjcook93@gmail.com>
2023-05-18 22:16:12 -0500

    add: death house section to homepage

--- src/components/pages/home/DeathHouse.js
+++ src/components/pages/home/DeathHouse.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 function DeathHouse() {
   return (
-    <div className="death-house">
-      Death House Placeholder
-
-      <div className="description__game">
-        <a href="https://death-house.netlify.app" target="_blank" className="description__game-link">
-          <img className="description__game-img" src="https://i.imgur.com/j8zRB5s.png" />
-        </a>
+    <a href="https://death-house.netlify.app" target="_blank" className="death-house">
+      <div className="death-house__textbox">
+        <span className="death-house__title heading-tertiary">Play Curse of Strahd: <br />Durst Manor</span>
+        <br />
+        <br />
+        <span className="death-house__subtitle">A recreation of the first chapter of Plight of the Sunseekers.  
+        <br /><br />
+        Will you survive this choose-your-own-adventure game?</span>
       </div>
-    </div>
+    </a>
   )
 }
 
