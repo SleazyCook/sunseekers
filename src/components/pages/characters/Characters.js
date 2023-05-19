e2d09fb05931fa07cdb3723571ccf80aa186a648
Andrew Cook <andrewjcook93@gmail.com>
2023-05-19 15:13:05 -0500

    refactor: characters.map() added

--- src/components/pages/characters/Characters.js
+++ src/components/pages/characters/Characters.js
@@ -5,6 +5,8 @@ import Header from '../../Header'
 import Navigation from '../../Navigation'
 import Footer from '../../Footer'
 
+import charactersData from './CharactersData'
+
 function Characters() {
 
   useEffect(() => {
@@ -24,136 +26,20 @@ function Characters() {
 
       <div className="flexbox">
 
-        <div className="flex__item">
-          <Link to="sunseekers">
-            <div className="cat-card cat-card--sunseekers">
-              <div className="cat-card__text-box">
-                <h3 className="cat-card__title">Sunseekers</h3>
-              </div>
+        {charactersData.map((charactersObj) => {
+          return (
+            <div className="flex__item">
+              <Link to={`${charactersObj.link}`}>
+                <div className={`cat-card cat-card--characters-${charactersObj.number}`}>
+                  <div className="cat-card__text-box">
+                    <h3 className="cat-card__title">{charactersObj.name}</h3>
+                  </div>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-
-        <div className="flex__item">
-          <Link to="castle-ravenloft">
-            <div className="cat-card cat-card--ravenloft">
-              <div className="cat-card__text-box">
-                <h3 className="cat-card__title">Castle Ravenloft</h3>
-              </div>
-            </div>
-          </Link>
-        </div>
-
-        <div className="flex__item">
-          <Link to="village-of-barovia">
-            <div className="cat-card cat-card--barovia">
-              <div className="cat-card__text-box">
-                <h3 className="cat-card__title">Village of Barovia</h3>
-              </div>
-            </div>
-          </Link>
-        </div>
-
-        <div className="flex__item">
-          <Link to="vistani">
-            <div className="cat-card cat-card--vistani">
-              <div className="cat-card__text-box">
-                <h3 className="cat-card__title">Vistani</h3>
-              </div>
-            </div>
-          </Link>
-        </div>
-
-        <div className="flex__item">
-          <Link to="keepers-of-the-feather">
-            <div className="cat-card cat-card--keepers">
-              <div className="cat-card__text-box">
-                <h3 className="cat-card__title">Keepers of the Feather</h3>
-              </div>
-            </div>
-          </Link>
-        </div>
-
-        <div className="flex__item">
-          <Link to="order-of-the-silver-dragon">
-            <div className="cat-card cat-card--dragon">
-              <div className="cat-card__text-box">
-                <h3 className="cat-card__title">Order of the Silver Dragon</h3>
-              </div>
-            </div>
-          </Link>
-        </div>
-
-        <div className="flex__item">
-          <Link to="church-of-the-morninglord">
-            <div className="cat-card cat-card--church">
-              <div className="cat-card__text-box">
-                <h3 className="cat-card__title">Church of the Morning Lord</h3>
-              </div>
-            </div>
-          </Link>
-        </div>
-
-        <div className="flex__item">
-          <Link to="the-baron">
-            <div className="cat-card cat-card--baron">
-              <div className="cat-card__text-box">
-                <h3 className="cat-card__title">The Baron</h3>
-              </div>
-            </div>
-          </Link>
-        </div>
-
-        <div className="flex__item">
-          <Link to="wachterhaus">
-            <div className="cat-card cat-card--wachterhaus">
-              <div className="cat-card__text-box">
-                <h3 className="cat-card__title">Wachterhaus</h3>
-              </div>
-            </div>
-          </Link>
-        </div>
-
-        <div className="flex__item">
-          <Link to="vallaki-merchants">
-            <div className="cat-card cat-card--merchants">
-              <div className="cat-card__text-box">
-                <h3 className="cat-card__title">Vallaki Merchants</h3>
-              </div>
-            </div>
-          </Link>
-        </div>
-
-        <div className="flex__item">
-          <Link to="vallaki-citizens">
-            <div className="cat-card cat-card--vallaki">
-              <div className="cat-card__text-box">
-                <h3 className="cat-card__title">Vallaki Citizens</h3>
-              </div>
-            </div>
-          </Link>
-        </div>
-
-        <div className="flex__item">
-          <Link to="witches-and-creatures">
-            <div className="cat-card cat-card--witches">
-              <div className="cat-card__text-box">
-                <h3 className="cat-card__title">Witches & Creatures</h3>
-              </div>
-            </div>
-          </Link>
-        </div>
-
-        <div className="flex__item">
-          <Link to="unknown-alliance">
-            <div className="cat-card cat-card--unknown">
-              <div className="cat-card__text-box">
-                <h3 className="cat-card__title">Unknown Alliance</h3>
-              </div>
-            </div>
-          </Link>
-        </div>
-
+          )
+        })}
+        
       </div> {/* Flexbox End */}
 
       <Footer />
---
+++ src/components/pages/characters/CharactersData.js
@@ -0,0 +1,74 @@
+let charactersData = [
+  {
+    number: 1,
+    name: "Sunseekers",
+    link: "/notes/characters/sunseekers"
+  },
+  {
+    number: 2,
+    name: "Castle Ravenloft",
+    link: "/notes/characters/castle-ravenloft"
+  },
+  {
+    number: 3,
+    name: "Village of Barovia",
+    link: "/notes/characters/village-of-barovia"
+  },
+  {
+    number: 4,
+    name: "Vistani",
+    link: "/notes/characters/vistani"
+  },
+  {
+    number: 5,
+    name: "Keepers of the Feather",
+    link: "/notes/characters/keepers-of-the-feather"
+  },
+  {
+    number: 6,
+    name: "Order of the Silver Dragon",
+    link: "/notes/characters/order-of-the-silver-dragon"
+  },
+  {
+    number: 7,
+    name: "Church of the Morning Lord",
+    link: "/notes/characters/church-of-the-morninglord"
+  },
+  {
+    number: 8,
+    name: "The Baron",
+    link: "/notes/characters/the-baron"
+  },
+  {
+    number: 9,
+    name: "Wachterhaus",
+    link:"/notes/characters/wachterhaus"
+  },
+  {
+    number: 10,
+    name: "Vallaki Merchants",
+    link: "/notes/characters/vallaki-merchants"
+  },
+  {
+    number: 11,
+    name: "Vallaki Citizens",
+    link: "/notes/characters/vallaki-citizens"
+  },
+  {
+    number: 12,
+    name: "Witches & Cults",
+    link: "/notes/characters/witches-and-cults"
+  },
+  {
+    number: 13,
+    name: "Unknown Alliance",
+    link: "/notes/characters/unknown-alliance"
+  },
+  {
+    number: 14,
+    name: "Creatures",
+    link: "/notes/characters/creatures"
+  }
+]
+
+export default charactersData;
