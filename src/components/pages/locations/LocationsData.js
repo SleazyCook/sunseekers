73dfdec9490d5bb3d2af8f8eea0776b283670d32
Andrew Cook <andrewjcook93@gmail.com>
2023-05-19 14:11:09 -0500

    refactor: location.map() added

---
+++ src/components/pages/locations/LocationsData.js
@@ -0,0 +1,114 @@
+let locationsData = [
+  {
+    number: 1,
+    name: "Mists of Barovia",
+    img: ""
+  },
+  {
+    number: 2,
+    name: "Old Svalich Road",
+    img: ""
+  },
+  {
+    number: 3,
+    name: "Svalich Woods",
+    img: ""
+  },
+  {
+    number: 4, 
+    name: "River Ivlis",
+    img: ""
+  },
+  {
+    number: 5,
+    name: "Village of Barovia",
+    img: ""
+  },
+  {
+    number: 6,
+    name: "Tser Pool Encampment",
+    img: ""
+  },
+  {
+    number: 7,
+    name: "Black Carriage",
+    img: ""
+  },
+  {
+    number: 8,
+    name: "Castle Ravenloft",
+    img: ""
+  },
+  {
+    number: 9,
+    name: "Old Mill",
+    img: ""
+  },
+  {
+    number: 10,
+    name: "Vallaki",
+    img: ""
+  },
+  {
+    number: 11,
+    name: "Vallaki Vistani Camp",
+    img: ""
+  },
+  {
+    number: 12,
+    name: "Lake Zarovich",
+    img: ""
+  },
+  {
+    number: 13,
+    name: "Mount Baratok",
+    img: ""
+  },
+  {
+    number: 14,
+    name: "Argonvostholt",
+    img: ""
+  },
+  {
+    number: 15,
+    name: "Village of Krezk",
+    img: ""
+  },
+  {
+    number: 16,
+    name: "Tsolenka Pass",
+    img: ""
+  },
+  {
+    number: 17,
+    name: "Ruins of Berez",
+    img: ""
+  },
+  {
+    number: 18,
+    name: "Van Richten's Tower",
+    img: ""
+  },
+  {
+    number: 19,
+    name: "Wizard of Wines",
+    img: ""
+  },
+  {
+    number: 20,
+    name: "Amber Temple",
+    img: ""
+  },
+  {
+    number: 21,
+    name: "Yester Hill",
+    img: ""
+  },
+  // {
+  //   number: 21,
+  //   name: "Werewolf Den",
+  //   img: ""
+  // }
+]
+
+export default locationsData;
