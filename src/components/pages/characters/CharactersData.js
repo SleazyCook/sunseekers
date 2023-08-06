673a3c2d7828b02cb48c75e0ed61d6209a42cb05
Andrew Cook <andrewjcook93@gmail.com>
2023-08-05 22:54:23 -0500

    update: characterData, Order of the Silver Dragon

--- src/components/pages/characters/CharactersData.js
+++ src/components/pages/characters/CharactersData.js
@@ -995,8 +995,17 @@ let charactersData = [
         img: "https://i.imgur.com/gMBEsmH.png",
         title: "Founder of Order of the Silver Dragon",
         race: "Silver Dragon",
+        age: "Unknown",
         firstAppearance: 14,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut odio dolor. Integer vitae sodales magna, nec consequat ante. Maecenas laoreet scelerisque iaculis. Phasellus vel."
+        description: "Argynvost was once a great knight and silver dragon. He founded Argynvostholt and the Order of the Silver Dragon to protect Barovia from the evil power of the Amber Temple.  Argynvost fought against Strahd's army but was ultimately slain by Strahd himself",
+        sections: [
+          {
+            id: 1,
+            name: "Head of the Order",
+            description: "When Argynvost fell in his battle against Strahd his head, in dragon form, was kept as a trophy for the invader. The Order of the Silver Dragon, throughout their curse as revenants, have sought after the skull of Argynvost for centuries",
+            localLinks: {}
+          }
+        ]
       },
       {
         id: 2,
@@ -1006,8 +1015,9 @@ let charactersData = [
         img: "https://i.imgur.com/8s2uE3q.png",
         title: "Silver Knight",
         race: "Revenant",
+        age: "Unknown",
         firstAppearance: 14,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut odio dolor. Integer vitae sodales magna, nec consequat ante. Maecenas laoreet scelerisque iaculis. Phasellus vel."
+        description: "Sir Vladimir Horngaard was a fierce knight in the Order of the Silver Dragon.  When Argynvost was slain by Strahd, Vladimir's bloodlust outlived him - turning not only him, but all of the knights within the Order, into revenants. Strahd sees them as a minor nuisance"
       },
       {
         id: 3,
@@ -1017,8 +1027,9 @@ let charactersData = [
         img: "https://i.imgur.com/I3Gc3uT.png",
         title: "Silver Knight",
         race: "Revenant",
+        age: "Unknown",
         firstAppearance: 14,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut odio dolor. Integer vitae sodales magna, nec consequat ante. Maecenas laoreet scelerisque iaculis. Phasellus vel."
+        description: "Sir Godfrey Gilwyn fought alongside Argynvost and Sir Vladimir Horngaard.  If Vladimir's hate and bloodlust brough the knights of the Order back from the death, it was Godfrey's love and patience that allowed a handful of knights to hold even the faintest hold on their humanity"
       },
       {
         id: 4,
@@ -1027,8 +1038,9 @@ let charactersData = [
         img: "https://i.imgur.com/0IVdyxP.png",
         title: "Silver Knight",
         race: "Revenant",
+        age: "Unknown",
         firstAppearance: 14,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut odio dolor. Integer vitae sodales magna, nec consequat ante. Maecenas laoreet scelerisque iaculis. Phasellus vel."
+        description: "Many knights from the Order of the Silver Dragon have descended completely into bloodthirsty revenants.  While they are fated to fight Strahd and his forces for all eternity, they will cut through anything, or anyone, that stands in their way"
       },
       {
         id: 5,
@@ -1037,8 +1049,9 @@ let charactersData = [
         img: "https://i.imgur.com/ESpelam.png",
         title: "Silver Knight",
         race: "Revenant",
+        age: "Unknown",
         firstAppearance: 14,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut odio dolor. Integer vitae sodales magna, nec consequat ante. Maecenas laoreet scelerisque iaculis. Phasellus vel."
+        description: "Sir Godfrey Gilwyn and a few other knights, while revenants, have been trying to hold on to what remains of their humanity.  They mostly stay in Argynvostholt to avoid any bloodthirsty tempation.  Their memories and the rest of what makes them more than monsters is fading away.  Their numbers are dwindling as more knights become monsters"
       },
       {
         id: 6,
@@ -1048,8 +1061,17 @@ let charactersData = [
         img: "https://i.imgur.com/akPmTiU.png",
         title: "Silver Knight",
         race: "Revenant",
+        age: "Unknown",
         firstAppearance: 14,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut odio dolor. Integer vitae sodales magna, nec consequat ante. Maecenas laoreet scelerisque iaculis. Phasellus vel."
+        description: "Sir Erich Norinthaal is one of the few remaining knights in Sir Godfrey's circle at Argynvostholt.  Following a theory from Sir Godfrey, his is on a mission to fulfill a passion from his mortal life to possibly end his undead existence. His mission is to finish his biggest hunt",
+        section: [
+          {
+            id: 1,
+            name: "Last Hunting Trip",
+            description: "The first of Sir Godfrey's last ditch efforts to aid his fellow knights in leaving the world of the Undead behind, Sir Erich joined the Sunseekers on a trip south towards ~Tsolenka Pass~ where they ultimately helped him slay the roc he hunted in his mortal life. He successfully passed on leaving behind his revenant curse.",
+            localLinks: {}
+          }
+        ]
       },
       {
         id: 7,
@@ -1059,8 +1081,9 @@ let charactersData = [
         img: "https://i.imgur.com/lslqYhu.png",
         title: "Silver Knight",
         race: "Revenant",
+        age: "Unknown",
         firstAppearance: 14,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut odio dolor. Integer vitae sodales magna, nec consequat ante. Maecenas laoreet scelerisque iaculis. Phasellus vel."
+        description: "Sir Damien Hargrove is one of the few remaining knights in Sir Godfrey's circle at Argynvostholt.  Following a theory from Sir Godfrey, his is on a mission to fulfill a passion from his mortal life to possibly end his undead existence. His mission is to offer his lost love the engagement ring he made for her."
       },
       {
         id: 8,
@@ -1070,8 +1093,9 @@ let charactersData = [
         img: "https://i.imgur.com/oicL9Ap.png",
         title: "Silver Knight",
         race: "Revenant",
+        age: "Unknown",
         firstAppearance: 14,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut odio dolor. Integer vitae sodales magna, nec consequat ante. Maecenas laoreet scelerisque iaculis. Phasellus vel."
+        description: "Sir Robern Boarwin is one of the few remaining knights in Sir Godfrey's circle at Argynvostholt.  Following a theory from Sir Godfrey, his is on a mission to fulfill a passion from his mortal life to possibly end his undead existence. His mission is pass on a family heirloom."
       },
       {
         id: 9,
@@ -1081,8 +1105,9 @@ let charactersData = [
         img: "https://i.imgur.com/TEMI1tp.png",
         title: "Silver Squire",
         race: "Revenant",
+        age: "Unknown",
         firstAppearance: 14,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut odio dolor. Integer vitae sodales magna, nec consequat ante. Maecenas laoreet scelerisque iaculis. Phasellus vel."
+        description: "Arthund Boarwin didn't get the chance to become a full-fledged knight.  Following a theory from Sir Godfrey, his is on a mission to fulfill a passion from his mortal life to possibly end his undead existence. His mission is to perform a truly heroic deed in order to become a proper knight of the Order."
       }
     ]
   },
