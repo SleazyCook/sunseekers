d6e0aed43b8b34a9ce8f1b2629a4041dd92bb43b
Andrew Cook <andrewjcook93@gmail.com>
2023-08-10 13:38:35 -0500

    fix: marketData created, resdeisnged component needed

---
+++ src/components/pages/market/MarketData.js
@@ -0,0 +1,146 @@
+let marketData = [
+  {
+    id: 1,
+    name: "Cracked Anvil",
+    shop: "Blacksmith",
+    owner: "Dragomir Valisov",
+    items: [
+      {
+        id: 1,
+        name: "Silver Coating",
+        buyPrice: "15 gp + 1 silver bar",
+        sellPrice: "N/A",
+        quantity: "N/A"
+      }
+    ]
+  },
+  {
+    id: 2, 
+    name: "Endless Delight Clothier",
+    shop: "Clothier",
+    owner: "Alastroi Siblings",
+    items: [
+      {
+        id: 1,
+        name: "Child's Winter Cloak",
+        buyPrice: "4 gp",
+        sellPrice: "2 gp",
+        quantity: "0"
+      },
+      {
+        id: 2,
+        name: "Gold Ring",
+        buyPrice: "10 gp",
+        sellPrice: "5 gp",
+        quantity: 1
+      },
+      {
+        id: 3,
+        name: "Topaz Necklace",
+        buyPrice: "100 gp",
+        sellPrice: "50 gp",
+        quantity: "0"
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: "Stefanovich Apotheocary",
+    shop: "Apthoethocary",
+    owner: "Harkus Stefanovich",
+    items: [
+      {
+        id: 1,
+        name: "Healing Potion",
+        buyPrice: "5 gp",
+        sellPrice: "2.5 gp",
+        quantity: 1
+      },
+      {
+        id: 2,
+        name: "Greater Healing Potion",
+        buyPrice: "20 gp",
+        sellPrice: "10 gp",
+        quantity: 1
+      },
+      {
+        id: 3,
+        name: "Mystery Potion",
+        buyPrice: "2 gp",
+        sellPrice: "1 gp",
+        quantity: 3
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: "Lukresh Tannery",
+    shop: "Tannery",
+    owner: "Fatima Lukresh",
+    items: [
+      {
+        id: 1,
+        name: "Wolf Pelt",
+        buyPrice: "0.20 gp",
+        sellPrice: "0.10 gp",
+        quantity: "10"
+      },
+      {
+        id: 2,
+        name: "Dire Wolf Pelt",
+        buyPrice: "0.50 gp",
+        sellPrice: "0.25 gp",
+        quantity: "10"
+      },
+      {
+        id: 3,
+        name: "Leather Bracers",
+        buyPrice: "2 gp",
+        sellPrice: "1 gp",
+        quantity: "20"
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: "Vallaki Wagon Repair",
+    shop: "Wagon Repair",
+    name: "Tural",
+    items: [
+
+    ]
+  },
+  {
+    id: 6,
+    name: "Odds N Ends",
+    shop: "General",
+    owner: "Bilbo",
+    items: [
+      {
+        id: 1,
+        name: "Blank Journal",
+        buyPrice: "0.80 gp",
+        sellPrice: "0.40 gp",
+        quantity: "0",
+      },
+      {
+        id: 2,
+        name: "Healing Kit",
+        buyPrice: "5 gp",
+        sellPrice: "2.5 gp",
+        quantity: "1"
+      }
+    ]
+  },
+  {
+    id: 7,
+    name: "The Straight Arrow",
+    shop: "Fletcher",
+    owner: "Ana Krogarov",
+    items: [
+
+    ]
+  }
+]
+
+export default marketData
